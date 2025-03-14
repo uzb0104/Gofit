eb4c503744b34d10dd8c38185c1421167da163d4
Omonov Abbosbek <abbosbekomonov04@gmail.com>
2025-03-14 06:37:25 +0500

    refactor: add Supabase configuration to .env and improve error handling in supabase client initialization

--- src/types/types/supabase.ts
+++ src/types/types/supabase.ts
@@ -1,12 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL!;
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || "";
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY || "";
 
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error(
-    "Supabase URL yoki API kalit topilmadi! `.env` faylini tekshiring."
-  );
-}
+console.log(supabaseUrl);
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
